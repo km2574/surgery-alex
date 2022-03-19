215fd37dad29d3726166064bf12cf2c16ad72f79
Kumar Manas <singularity@Kumars-MacBook-Pro.local>
2022-03-20 04:00:09 +0530

    hero button margin

--- Components/Herosection.js
+++ Components/Herosection.js
@@ -16,7 +16,7 @@ const Herosection = () => {
                         With Alex Health you can Compare prices & quality of hospitals, Get an estimated bill for all surgery and treatment, Know what’s covered in your insurance, Get final amount you pay
                         </p>
                         <br />
-                        <Button onClick={() => setModalShow(true)} size="lg" className="freecall border-0">
+                        <Button onClick={() => setModalShow(true)} size="lg" className=" my-3 freecall border-0">
                             <span className="fa fa-phone" style={{ fontSize: 20 }}> </span>
                             &nbsp;&nbsp;&nbsp;Book a free call
                         </Button>
